dc58b37fc60d7a4ffbeb21e0758ccf0c89f31a07
alyson.xu <alyson.xu@ucloud.cn>
2023-07-12 21:39:40 +0800

    'deepClone'

--- interview/JS/deepClone.js
+++ interview/JS/deepClone.js
@@ -1,20 +1,41 @@
+// typeof 识别所有的值类型（undefined、string、number、boolean、symbol），识别函数（function），判断是不是引用类型（object）
+// 
+
 function deepClone(obj){
     if(typeof obj !=='object' || obj==null){
         return obj;
     }
     let res;
     if(obj instanceof Array){
-        res=[];
+        res =[];
     }else{
         res={};
     }
     for(let key in obj){
         if(obj.hasOwnProperty(key)){
-            res[key]= deepClone(obj[key])
+            // 递归调用！！！
+            res[key] = deepClone(obj[key]);
         }
     }
     return res;
 }
+// function deepClone(obj){
+//     if(typeof obj !=='object' || obj==null){
+//         return obj;
+//     }
+//     let res;
+//     if(obj instanceof Array){
+//         res=[];
+//     }else{
+//         res={};
+//     }
+//     for(let key in obj){ 
+//         if(obj.hasOwnProperty(key)){
+//             res[key]= deepClone(obj[key])
+//         }
+//     }
+//     return res;
+// }
 
 const obj={
     name:'age',
